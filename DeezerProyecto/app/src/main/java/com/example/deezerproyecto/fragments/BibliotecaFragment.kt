133e8897c5b554c51616451f76a0c7b6d264df0e
Ruben <rubenlozano2003@gmail.com>
2025-05-15 14:52:36 +0200

    codigo

--- DeezerProyecto/app/src/main/java/com/example/deezerproyecto/fragments/BibliotecaFragment.kt
+++ DeezerProyecto/app/src/main/java/com/example/deezerproyecto/fragments/BibliotecaFragment.kt
@@ -1,22 +1,48 @@
 package com.example.deezerproyecto.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.deezerproyecto.R
+import com.example.deezerproyecto.adapters.PlaylistAdapter
 import com.example.deezerproyecto.databinding.FragmentBibliotecaBinding
+import com.example.deezerproyecto.models.Playlist
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class BibliotecaFragment : Fragment() {
 
     private var _binding: FragmentBibliotecaBinding? = null
     private val binding get() = _binding!!
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var playlistAdapter: PlaylistAdapter
+    private val playlists = mutableListOf<Playlist>()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         _binding = FragmentBibliotecaBinding.inflate(inflater, container, false)
+
+        // Inicialización del RecyclerView
+        recyclerView = binding.recyclerPlaylists
+        playlistAdapter = PlaylistAdapter(playlists) {
+            // Acción al hacer click en una playlist
+        }
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = playlistAdapter
+
+        // Botón para añadir nueva Playlist
+        binding.fabAddPlaylist.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.contenedorFragment, PlaylistFragment())
+                .addToBackStack(null)
+                .commit()
+        }
+
         return binding.root
     }
 
